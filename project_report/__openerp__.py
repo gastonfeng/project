c50bd4c26422d13f297ff86012f5120b7800ea20
valentinthirion <valentinthirion@gmail.com>
2016-10-13 11:56:14 +0200

    [FIX] project_report improved module desc file

--- project_report/__openerp__.py
+++ project_report/__openerp__.py
@@ -1,15 +1,15 @@
 {
     'name': "Project Report",
-    'version': '9.0.1.0',
-    'depends': ['report','project'],
+    'version': '9.0.1.0.0',
+    'depends': ['report', 'project'],
     'author': "Quentin Tawry, AbAKUS it-solutions PGmbH",
     'website': "http://www.abakusitsolutions.eu",
     'category': 'project',
-    'description': 
+    'description':
     """
 This modules adds a report and wizard on projects in order to print a beautiful report containing all information an data for this project.
 
-It is possible to see Issues, Tasks (summarized or fully detailed) as well as charts for some inf.
+It is possible to see Issues, Tasks (summarized or fully detailed) as well as charts for some info.
 
 This module has been developed by Quentin TAWRY @ AbAKUS it-solutions
     """,
