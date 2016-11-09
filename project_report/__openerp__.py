fadd363012cccedf8ad288bb4c3354702c1cb663
Valentin Thirion <vt@abakusitsolutions.eu>
2016-11-09 15:44:23 +0100

    [FIX] project_report Removed the comments in the report if empty

--- project_report/__openerp__.py
+++ project_report/__openerp__.py
@@ -1,6 +1,6 @@
 {
     'name': "Project Report",
-    'version': '9.0.1.0.2',
+    'version': '9.0.1.0.3',
     'depends': ['report', 'project'],
     'author': "Quentin Tawry, AbAKUS it-solutions PGmbH",
     'website': "http://www.abakusitsolutions.eu",
