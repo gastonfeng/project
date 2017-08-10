72647a9e05d7fb753e1d1210309a26151c595e0d
Valentin Thirion <vt@abakusitsolutions.eu>
2017-08-10 09:16:30 +0200

    [FIX] project_service_desk Corrected related field name for task/issue stage

--- project_service_desk/models/account_analytic_line.py
+++ project_service_desk/models/account_analytic_line.py
@@ -8,5 +8,5 @@ _logger = logging.getLogger(__name__)
 class account_analytic_line_service_desk(models.Model):
     _inherit = ['account.analytic.line']
 
-    issue_state = fields.Selection(related='issue_id.state', string="Issue State")
-    task_state = fields.Selection(related='task_id.state', string="Task State")
+    issue_state = fields.Many2one(related='issue_id.stage_id', string="Issue State")
+    task_state = fields.Many2one(related='task_id.stage_id', string="Task State")
