b9db6fcf5563e07676444ab4ead90c7bf4a480b9
Valentin Thirion <vt@abakusitsolutions.eu>
2018-12-12 15:26:40 +0100

    [ADD] First OK website version

--- project_smart_display/controllers/main.py
+++ project_smart_display/controllers/main.py
@@ -14,7 +14,46 @@ class SmartDisplayPage(http.Controller):
 
     @http.route([
         '/smartdisplay/display/<model("project.smart.display"):display_id>',
-    ], type='http', auth="user", website=True)
+    ], type='http', auth="public", website=True)
     def smart_display_web_page(self, page=0, display_id=None, search='', ppg=False, **post):
         display_ids = http.request.env['project.smart.display'].sudo().search([('id', '=', display_id.id)])
-        return http.request.render('project_smart_display.smart_display_web_page', {'display': display_ids})
+        return http.request.render('project_smart_display.smart_display_web', {'display': display_ids})
+
+    @http.route([
+        '/smartdisplay/getdisplay/'],
+                type='json', auth="public", methods=['POST'], website=True, csrf=False)
+    def get_display(self, display_id, *kw):
+        """ Ajax call to get page from id """
+        if display_id:
+            display = request.env['project.smart.display'].sudo().search([('id', '=', display_id)])
+            
+            if not display or len(display) != 1:
+                return {'display_id': -1}
+            else:
+                return {
+                    'display_id': display.id,
+                    'name': display.name,
+                    'page_count': len(display.page_ids),
+                }
+
+    @http.route([
+        '/smartdisplay/getnextpage/'],
+                type='json', auth="public", methods=['POST'], website=True, csrf=False)
+    def get_next_page(self, display_id, index, *kw):
+        """ Ajax call to get page for display """
+        if display_id:
+            display = request.env['project.smart.display'].sudo().search([('id', '=', display_id)])
+            if not display or len(display) != 1 or len(display.page_ids) == 0:
+                return {'page_id': -1}
+            if index < 0:
+                index = 0
+            if index >= len(display.page_ids):
+                index = 0
+            next_page = display.page_ids[index]
+            return {
+                'page_id': next_page.id,
+                'name': next_page.name,
+                'sequence': next_page.sequence,
+                'mode': next_page.mode,
+                'iframe_url': next_page.iframe_url,
+            }
--- project_smart_display/static/src/js/smart_display.js
+++ project_smart_display/static/src/js/smart_display.js
@@ -16,3 +16,72 @@ function checkTime(i) {
     if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
     return i;
 }
+
+var delay = ( function() {
+    var timer = 0;
+    return function(callback, ms) {
+        clearTimeout (timer);
+        timer = setTimeout(callback, ms);
+    };
+})();
+
+function startPageManager() {
+    odoo.define('project_smart_display.smart_display_viewing', ['web.ajax'], function (require) {
+        "use strict";
+        var ajax = require('web.ajax');
+
+        var display_id = parseInt($("#display_id").text());
+
+        var page_count = parseInt($("#page_count").text()); // TODO replace this by a JSON REQUEST
+
+        // Get first page
+        delay(function(){
+            ajax.jsonRpc("/smartdisplay/getdisplay/", 'call', {
+                'display_id': display_id,
+            }).then(function (data_display) {
+                if(data_display){
+                    ajax.jsonRpc("/smartdisplay/getnextpage/", 'call', {
+                        'display_id': data_display['display_id'],
+                        'index': 0,
+                    }).then(function (data_page) {
+                        if(data_page){
+                            if (data_page['mode'] == 'iframe') {
+                                document.getElementById('page_iframe').setAttribute('src', data_page['iframe_url']);
+                                document.getElementById('page_iframe').style.display = "inline";
+                            }
+                        }
+                    })
+                }
+            })
+        });
+
+        setInterval(function() { counter() }, 30000); // And wait 30s TODO : set this param dynamically
+
+        function counter() {
+            var index = parseInt($("#page_index").text());
+
+            // Get page for index
+            ajax.jsonRpc("/smartdisplay/getnextpage/", 'call', {
+                'display_id': display_id,
+                'index': index,
+            }).then(function (data_page) {
+                if(data_page){
+                    if (data_page['mode'] == 'iframe') {
+                        document.getElementById('page_iframe').setAttribute('src', data_page['iframe_url']);
+                        document.getElementById('page_iframe').style.display = "inline";
+                    }
+                }
+            })
+
+            // Increase index
+            if (index < page_count) {
+                index = index + 1;
+            }
+            else {
+                index = 1;
+            }
+            $("#page_index").text(index);
+
+        }
+    });
+}
